86c9e8cf1cebcf88a010a87f4e486f9282d99fe7
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-11-10 16:06:56 +0400

    Correct body

--- src/test/java/netology/ru/rest/PostmanEchoTEST.java
+++ src/test/java/netology/ru/rest/PostmanEchoTEST.java
@@ -15,7 +15,7 @@ class PostmanEchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("invalid") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
